958713a057aff03b31d09a72872ec457c7dabed5
Maram Abu Murad <maramabumurad97@gmail.com>
2021-10-26 23:39:50 +0300

    first lesson

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.titleText}>Hello World!</Text>
+      <View>
+        <Text style={styles.titleText}>Enter your name :</Text>
+        < TextInput
+          placeholder='e.g Maram'
+          style={styles.input}
+          onChangeText={(value) => setName(value)}
+          multiline />
+      </View>
+
+      <View>
+        <Text style={styles.titleText}>Enter your age :</Text>
+        < TextInput
+          placeholder='e.g 24'
+          style={styles.input}
+          onChangeText={(value) => setAge(value)}
+          keyboardType= 'numeric' />
+      </View>
+      
+      <View>
+        <Text style={styles.titleText}> Welcome {name} , your age is : {age} </Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,6 +42,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
-    color:'#fff'
   },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: 'red'
+  },
+  input: {
+    borderRadius: 5,
+    borderColor: '#777',
+    borderWidth: 1,
+    color: '#fff',
+    padding:5,
+    margin:5
+  }
 });
